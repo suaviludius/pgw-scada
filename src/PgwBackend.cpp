ffb10f554d89cb9c76d6b0703c5c389a91547c2d
Artem <powerranger1912@gmail.com>
2026-06-24 23:56:02 +0300

    feat(pgw-backend): добавлены сигналы для связи с TcpClient и методы для использования в qml

--- src/PgwBackend.cpp
+++ src/PgwBackend.cpp
@@ -4,7 +4,17 @@
 PgwBackend::PgwBackend(QObject* parent)
     : QObject(parent)
 {
-    // Инициализация
+    // Подключаем к TcpClient сигналы к слотам PgwBackend
+    connect(&m_tcpClient, &TcpClient::connectedChanged, this, &PgwBackend::connectedChanged);
+}
+
+bool PgwBackend::connectToServer(const QString& host, quint16 port){
+    if(!m_tcpClient.isConnected())
+    return m_tcpClient.connectToServer(host, port);
+}
+
+void PgwBackend::disconnectFromServer() {
+    m_tcpClient.disconnectFromServer();
 }
 
 PgwBackend::~PgwBackend()
--- src/PgwBackend.h
+++ src/PgwBackend.h
@@ -2,25 +2,41 @@
 #ifndef PGW_BACKEND_H
 #define PGW_BACKEND_H
 
+#include "TcpClient.h"
+
 #include <QObject>
 
 
 class PgwBackend : public QObject {
     Q_OBJECT
-    Q_PROPERTY(bool isConnected READ isConnected) //NOTIFY connectedChanged)
+    // Свойство, создает NOTIFY при вызове сигнала connectedChanged
+    Q_PROPERTY(bool isConnected READ isConnected NOTIFY connectedChanged)
 
 public:
     explicit PgwBackend(QObject* parent = nullptr);
     ~PgwBackend() override;
 
+    // QML методы
+    // Подключение к TCP серверу
+    Q_INVOKABLE bool connectToServer(const QString& host, quint16 port);
+    // Отключение от TCP сервера
+    Q_INVOKABLE void disconnectFromServer();
+
+    // Геттеры для Q_PROPERTY
     // Проверка подключения к серверу
-    bool isConnected() const { return false; }
+    bool isConnected() const { return m_tcpClient.isConnected();  }
 
 signals:
-// Все сигналы проперти нужно объявить здесь
+    // Сигнал изменения подключения к TcpServer
+    // Данный сигнал служит слотом для сигнала connectedChanged от TcpClient
+    void connectedChanged();
+
 private slots:
+    // Тут мог быть приемник connectedChanged от TcpClient
 
 private:
+    // Клиент для подключения к TCP серверу PGW
+    TcpClient m_tcpClient;
 
 };
 
